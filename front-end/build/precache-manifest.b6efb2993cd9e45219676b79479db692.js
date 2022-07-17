b6cd863fe5c543d566e13e051f7476a52c6beeae
Ronin Sharma <rrs234@cornell.edu>
2022-07-17 00:02:09 -0400

    added basic html file

---
+++ front-end/build/precache-manifest.b6efb2993cd9e45219676b79479db692.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0751f782de5d5ef4596554453b293c6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4479385a2e578c0b7314",
+    "url": "/static/css/main.3378b176.chunk.css"
+  },
+  {
+    "revision": "313c469f353c6b8b9be2",
+    "url": "/static/js/2.294321b3.chunk.js"
+  },
+  {
+    "revision": "ea2e67682ff58355750c2a960c8ac43e",
+    "url": "/static/js/2.294321b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad2a6f8efe6adc1a9fac",
+    "url": "/static/js/3.756e27d3.chunk.js"
+  },
+  {
+    "revision": "4479385a2e578c0b7314",
+    "url": "/static/js/main.5cfecd8a.chunk.js"
+  },
+  {
+    "revision": "cc61818a7a1c94fdadc9",
+    "url": "/static/js/runtime-main.b9a951e5.js"
+  },
+  {
+    "revision": "2d11e88e67af35e35d2eb547ae4f74c9",
+    "url": "/static/media/Allison_Hutchison_Headshot.2d11e88e.png"
+  },
+  {
+    "revision": "13dc4df4d2f85bef0e61cb44b71694ee",
+    "url": "/static/media/Danielle_Simon_Headshot.13dc4df4.png"
+  },
+  {
+    "revision": "3a5e8eef61823895fdb605821453596a",
+    "url": "/static/media/Neil_Headshot.3a5e8eef.jpg"
+  },
+  {
+    "revision": "ee128b682138ebce40c8ed398cc982c7",
+    "url": "/static/media/Nina_Headshot.ee128b68.JPG"
+  },
+  {
+    "revision": "2cec029a2de3f1ab5d97be83ebe402bf",
+    "url": "/static/media/Paul_Headshot.2cec029a.jpeg"
+  },
+  {
+    "revision": "73f5a2225986c86bcfd1a32355a0fb99",
+    "url": "/static/media/Ronin_Headshot.73f5a222.jpg"
+  },
+  {
+    "revision": "3e9ffe022e254ac7f816daf7f8bd9e95",
+    "url": "/static/media/home_page_pic.3e9ffe02.png"
+  },
+  {
+    "revision": "4ec58bf13626cda3c54cc966a0b47dd9",
+    "url": "/static/media/skill_search_pic.4ec58bf1.png"
+  }
+]);
